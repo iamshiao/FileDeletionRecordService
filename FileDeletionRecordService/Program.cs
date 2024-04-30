a4dac44dec91d106c5dd87f7d842dbc1c866e534
Circle Hsiao <circle.hsiao1@hp.com>
2024-04-30 17:14:35 +0800

    Init solution

---
+++ FileDeletionRecordService/Program.cs
@@ -0,0 +1,10 @@
+using FileDeletionRecordService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
