75ccec989f0f80359f7d023d0c3a581c384f2c5a
Circle Hsiao <circle.hsiao1@hp.com>
2024-05-01 12:39:59 +0800

    Draft the basic based on MSDN sample

--- FileDeletionRecordService/Program.cs
+++ FileDeletionRecordService/Program.cs
@@ -1,8 +1,18 @@
 using FileDeletionRecordService;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddWindowsService(options =>
+        {
+            options.ServiceName = ".NET Joke Service";
+        });
+
+        LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(services);
+
+        services.AddSingleton<JokeService>();
         services.AddHostedService<Worker>();
     })
     .Build();
--- FileDeletionRecordService/Worker.cs
+++ FileDeletionRecordService/Worker.cs
@@ -1,20 +1,46 @@
 namespace FileDeletionRecordService
 {
-    public class Worker : BackgroundService
+    public sealed class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly JokeService _jokeService;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(JokeService jokeService, ILogger<Worker> logger)
         {
+            _jokeService = jokeService;
             _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    string joke = _jokeService.GetJoke();
+                    _logger.LogWarning("{Joke}", joke);
+
+                    await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // When the stopping token is canceled, for example, a call made from services.msc,
+                // we shouldn't exit with a non-zero exit code. In other words, this is expected...
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{Message}", ex.Message);
+
+                // Terminates this process and returns an exit code to the operating system.
+                // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+                // performs one of two scenarios:
+                // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+                // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+                //
+                // In order for the Windows Service Management system to leverage configured
+                // recovery options, we need to terminate the process with a non-zero exit code.
+                Environment.Exit(1);
             }
         }
     }
